b3fa72c9fe00908c91804de79039f7c1642fb029
Ajay Neethi Kannan <35770004+Ajayneethikannan@users.noreply.github.com>
2018-10-21 08:59:26 +0530

    checking wifi p2p

---
+++ app/src/main/java/com/example/ajaynk/peoplefisher2/WifiDirectBroadcastReceiver.java
@@ -0,0 +1,73 @@
+package com.example.ajaynk.peoplefisher2;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.net.NetworkInfo;
+import android.net.wifi.WifiManager;
+import android.net.wifi.p2p.WifiP2pManager;
+import android.widget.Toast;
+
+public class WifiDirectBroadcastReceiver extends BroadcastReceiver {
+    private WifiP2pManager mManager;
+    private WifiP2pManager.Channel mChannel;
+    private MainActivity mActivity;
+
+    public WifiDirectBroadcastReceiver(WifiP2pManager mManager, WifiP2pManager.Channel mChannel, MainActivity mainActivity)
+    {
+        this.mManager = mManager;
+        this.mChannel = mChannel;
+        this.mActivity = mainActivity;
+    }
+
+
+    @Override
+    public void onReceive(Context context, Intent intent)
+    {
+        String action = intent.getAction();
+
+        if (WifiP2pManager.WIFI_P2P_STATE_CHANGED_ACTION.equals(action)) {
+            // Check to see if Wi-Fi is enabled and notify appropriate activity
+            int state = intent.getIntExtra(WifiP2pManager.EXTRA_WIFI_STATE,-1);
+            if(state == WifiP2pManager.WIFI_P2P_STATE_ENABLED)
+            {
+                Toast.makeText(context, "Wifi is ONNNN!", Toast.LENGTH_SHORT).show();
+
+            }
+            else
+            {
+                Toast.makeText(context, "Wifi is OFFF!", Toast.LENGTH_SHORT).show();
+
+            }
+        } else if (WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION.equals(action)) {
+            // Call WifiP2pManager.requestPeers() to get a list of current peers
+            if(this.mManager != null)
+            {
+                Toast.makeText(context, "Wifi !", Toast.LENGTH_SHORT).show();
+                this.mManager.requestPeers(this.mChannel, this.mActivity.peerListListener);
+            }
+        } else if (WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION.equals(action)) {
+            if(mManager == null)
+            {
+                return;
+            }
+
+            NetworkInfo networkInfo = intent.getParcelableExtra(WifiP2pManager.EXTRA_NETWORK_INFO);
+
+            if(networkInfo.isConnected())
+            {
+                  mManager.requestConnectionInfo(mChannel, mActivity.connectionInfoListener);
+            }
+            else
+            {
+                mActivity.connectionStatus.setText("Device disconnected");
+            }
+
+        } else if (WifiP2pManager.WIFI_P2P_THIS_DEVICE_CHANGED_ACTION.equals(action)) {
+            // Respond to this device's wifi state changing
+        }
+    }
+
+
+
+}
